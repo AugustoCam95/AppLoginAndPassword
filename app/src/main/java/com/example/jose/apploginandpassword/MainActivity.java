977757457147e212f2101c7ee6dca5f302ef2792
augustocam95@gmail.com <augustocam95@gmail.com>
2018-04-15 23:07:31 -0300

    Tentativa XD

--- app/src/main/java/com/example/jose/apploginandpassword/MainActivity.java
+++ app/src/main/java/com/example/jose/apploginandpassword/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.jose.apploginandpassword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -31,17 +33,45 @@ public class MainActivity extends AppCompatActivity {
         TittleText = (EditText)findViewById(R.id.TittleText);
         SavePasstextView = (TextView)findViewById(R.id.SavePassButton);
 
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-
+        loginButton.setOnClickListener(this);
+        SavePassButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view){
+
+        boolean SameLogin, SamePass;
+
+        switch (view.getId()) {
+            case R.id.loginButton:
+                SameLogin = Boolean.parseBoolean(String.valueOf(loginText.getText().equals(user1.login)));
+                Log.v("LOG", String.valueOf(SameLogin));
+                SamePass = Boolean.parseBoolean(String.valueOf(passText.getText().equals(user1.password)));
+                Log.v("LOG", String.valueOf(SamePass));
+
+                if (SameLogin && !SamePass) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Insira o login correspodente", Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.v("LOG", "Errou a senha");
+                }
+
+                if (!SameLogin && SamePass) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Insira o login correspodente", Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.v("LOG", "Errou o login");
+                }
+
+                if (!SameLogin && !SamePass) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Insira o login correspodente", Toast.LENGTH_LONG);
+                    toast.show();
+                    Log.v("LOG", "Errou login e senha");
+                }
+                break;
+
+            case R.id.SavePassButton:
+                break;
 
 
+        }
+    }
 }
